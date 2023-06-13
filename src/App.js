4bd11d4eba9c44b6ef84ab3f1547080fa5ec2a50
Kian Malakooti <kianjmalakooti@gmail.com>
2023-06-12 23:14:57 -0700

    Fallback route (maybe fixes non loading page?)

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="portfolio/:name" element={<PortfolioImages />} />
+            <Route path="*" element={<Loader />} />
           </Routes>
         </Suspense>
       </Router>
