8057f8bc60b1d8c38f14c1056a340054ccbc5f40
Kian Malakooti <kianjmalakooti@gmail.com>
2023-06-12 19:46:49 -0700

    Fixed image modal popup

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,31 +13,29 @@ const Portfolio = () => {
         Portfolio 📸
       </h2>
       <div className="flex justify-center md:flex-col md:items-center lg:w-[80%]">
-        {SECTIONIMAGES.slice(0, 3).map(
-          ({ id, title, name, description, detailedImages, path }) => (
-            <Link
-              to={`/portfolio/${name}`}
-              key={id}
-              className="p-2 relative cursor-pointer"
-            >
-              <IKImage
-                className="mb-4 rounded-xl"
-                urlEndpoint={urlEndpoint}
-                path={path}
-                loading="lazy"
-                transformation={[
-                  {
-                    quality: 100,
-                    height: 400,
-                  },
-                ]}
-              />
-              <p className="absolute bottom-6 right-4 p-2 text-2xl sm:text-lg font-semibold text-white">
-                {title}
-              </p>
-            </Link>
-          )
-        )}
+        {SECTIONIMAGES.slice(0, 3).map(({ id, title, name, path }) => (
+          <Link
+            to={`/portfolio/${name}`}
+            key={id}
+            className="p-2 relative cursor-pointer"
+          >
+            <IKImage
+              className="mb-4 rounded-xl"
+              urlEndpoint={urlEndpoint}
+              path={path}
+              loading="lazy"
+              transformation={[
+                {
+                  quality: 100,
+                  height: 400,
+                },
+              ]}
+            />
+            <p className="absolute bottom-6 right-4 p-2 text-2xl sm:text-lg font-semibold text-white">
+              {title}
+            </p>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/pages/PortfolioImages.jsx
+++ src/pages/PortfolioImages.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import SECTIONIMAGES from "../components/PortfolioImageData";
 import { IKImage } from "imagekitio-react";
@@ -9,52 +9,42 @@ const PortfolioImages = () => {
   const { name } = useParams();
   const location = SECTIONIMAGES.find((location) => location.name === name);
 
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (path) => {
+    setSelectedImage(path);
+    console.log(path);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   const renderDetailedImages = () => {
-    if (location.detailedImages.length === 5) {
-      // Render images in a row
-      return (
-        <div className="flex justify-center">
-          {location.detailedImages.map(({ id, path }) => (
-            <div key={id} className="p-2 cursor-pointer">
-              <IKImage
-                className="mb-4 rounded-xl"
-                urlEndpoint={urlEndpoint}
-                path={path}
-                loading="lazy"
-                transformation={[
-                  {
-                    quality: 100,
-                    height: 300,
-                  },
-                ]}
-              />
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      // Render images in a grid when more than 5 images
-      return (
-        <div className="grid grid-cols-4 justify-center w-[80%]">
-          {location.detailedImages.map(({ id, path }) => (
-            <div key={id} className="p-2 cursor-pointer">
-              <IKImage
-                className="rounded-xl"
-                urlEndpoint={urlEndpoint}
-                path={path}
-                loading="lazy"
-                transformation={[
-                  {
-                    width: 400,
-                    height: 400,
-                  },
-                ]}
-              />
-            </div>
-          ))}
-        </div>
-      );
-    }
+    return (
+      <div className="grid grid-cols-4 justify-center w-[80%]">
+        {location.detailedImages.map(({ id, path }) => (
+          <div
+            key={id}
+            className="p-2 cursor-pointer"
+            onClick={() => handleImageClick(path)}
+          >
+            <IKImage
+              className="rounded-xl"
+              urlEndpoint={urlEndpoint}
+              path={path}
+              loading="lazy"
+              transformation={[
+                {
+                  width: 400,
+                  height: 400,
+                },
+              ]}
+            />
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
@@ -65,7 +55,27 @@ const PortfolioImages = () => {
       <p className="w-full font-semibold text-3xl text-center mb-20">
         {location.description}
       </p>
+
       {renderDetailedImages()}
+
+      {selectedImage && (
+        <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-black bg-opacity-80">
+          <div className="relative max-w-3xl mx-auto">
+            <button
+              className="absolute top-4 right-4 p-2 text-white rounded-full bg-gray-800"
+              onClick={handleCloseModal}
+            >
+              X
+            </button>
+            <IKImage
+              className="max-w-full max-h-full"
+              urlEndpoint={urlEndpoint}
+              path={selectedImage}
+              loading="lazy"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
